164eed8dd6118cf1727378b6e12e398696019586
Dmitriy Bezrukov <94968321+dimetcb@users.noreply.github.com>
2022-04-26 08:57:02 +0300

    Обнавлена форма авторизации пользователя
    1.Выполняется запрос на сервер.
    2. Проверяется есть ли такой пользователь, в случаи успеха открывается доступ.

--- autoriz_obr.php
+++ autoriz_obr.php
@@ -1,9 +1,24 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-$email = $_POST['name_email'];
-$pass = $_POST['name_pass'];
+  header('Content-Type: text/html; charset=utf-8');
 
-echo "Email: $email<br>
-Пароль: $pass";
+  $mysqli = mysqli_connect("localhost", "oiujmjkg_firstbd", "12345", "oiujmjkg_firstbd");
+    if ($mysqli == false) {
+      print("error");
+  } else {
+
+  $email = $_POST['email'];
+  $pass = $_POST['password'];
+
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'  AND `password` = '$pass'");
+
+  if ($result->num_rows !== 0) {
+    print("ok");
+  } else {
+    print("error");
+  }
+}
+
+// echo "Email: $email<br>
+//  Пароль: $pass";
 
 ?>
